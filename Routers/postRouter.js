82f659de66b7242d53065563c269af27e13a7deb
Mai Abdallah <113540640+maiabdallah7@users.noreply.github.com>
2024-10-15 07:52:28 +0300

    Update postRouter.js

--- Routers/postRouter.js
+++ Routers/postRouter.js
@@ -5,24 +5,7 @@ const isAuth = require('../Middleware/isAuth');
 //const upload= require('../middlewares/uploadFile')
 const router = express.Router();
 
-// 
-/*router.post('/addPost',isAuth,  upload.array('files'), postController.addPost );
-router.put('/:id',isAuth, postController.updatePost);
-router.delete('/:id',isAuth, postController.deletePost);
-router.post('/:id/reactPost', isAuth, postController.reactPost);
-router.post('/:id/sharePost', isAuth, postController.sharePost);
-router.post('/:id/savePosts', isAuth, postController.savePosts);
 
-//Comment
-//router.get('/:postId/getPostComments' , isAuth, postController.getPostComments)
-router.post('/:id/addComment', isAuth,upload.array('files'), postController.addComment);
-router.put('/:postId/:commentId/updateComment', isAuth, postController.updateComment);
-router.delete('/:postId/:commentId/deleteComment', isAuth, postController.deleteComment);
-router.post('/:postId/commentId/reactComment', isAuth, postController.reactComment);
-//Reply
-router.post('/:postId/:commentId/replyComment', isAuth,upload.array('files'), postController.replyComment);
-router.post('/:postId/:commentId/:replyId/reactReply', isAuth, postController.reactReply);
-router.delete('/:postId/:commentId/:replyId/deleteReply', isAuth, postController.deleteRely);
-router.get('/:postId/comments/:commentId/replies' , isAuth , postController.getCommentReplies)*/
+
 
 module.exports = router;
